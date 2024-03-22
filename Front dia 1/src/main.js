8983f11e22f2a4fdaa0283a73018d91eaffe4681
Antonio Lindo <antoniolvitall@gmail.com>
2024-03-22 10:28:18 -0300

    com bug, corrigindo

--- Front dia 1/src/main.js
+++ Front dia 1/src/main.js
@@ -1,1 +1,20 @@
 const validator = require('validator');
+
+const input = document.getElementById('input');
+const btn = document.getElementById('btn');
+const select = document.getElementById('select');
+const text = document.getElementById('resposta')
+
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const campos = {
+    cpf: validator.isTaxID(input.value, 'pt-BR'),
+    hexColor: validator.isHexColor(input.value),
+    email: validator.isEmail(input.value),
+    uuid: validator.isUUID(input.value, 4),
+    url: validator.isURL(input.value),
+  };
+
+  text.innerHTML = `A validacao retornou ${campos[select.value]}`
+})
