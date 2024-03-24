6b16954a6ae1dc8a7b7ac2aa1f7328a5aa7fb7d6
Antonio Lindo <antoniolvitall@gmail.com>
2024-03-24 10:03:57 -0300

    finalizando

--- src/main.js
+++ src/main.js
@@ -3,15 +3,25 @@ import Swal from 'sweetalert2';
 const btn = document.getElementById('btn');
 const name = document.getElementById('nameHero');
 const img = document.getElementById('imgHero');
-const API_KEY = 'https://akabab.github.io/superhero-api/api';
 
 const randomNumber = () => Math.floor(Math.random() * 100) + 1;
 
-btn.addEventListener('click', generate);
+btn.addEventListener('click', async (event) => {
+    event.preventDefault
 
-const generate = async () => {
     try {
         const promise = await fetch(`https://akabab.github.io/superhero-api/api/id/${randomNumber()}.json`);
+        if (!promise.ok) {
+            throw new Error('Network response was not ok');
+          }
         const data = await promise.json();
-    }
-};
+        img.src = data.images.md;
+        name.innerHTML = data.name;
+    } catch(error) {
+        Swal.fire({
+            title: 'Hero not found',
+            text: error.message,
+            icon: 'error',
+            confirmButtonText: 'Cool',
+    })}
+});
